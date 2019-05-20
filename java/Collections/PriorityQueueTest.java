e34b6c917e1eefb47e1eaa5535437ec2d450a627
ZhouTsing <zhoutsing@foxmail.com>
2019-05-21 00:06:51 +0800

    Add PriorityQueueTest.java

--- java/Collections/PriorityQueueTest.java
+++ java/Collections/PriorityQueueTest.java
@@ -1,11 +1,15 @@
 package com.jay.test;
 
 import java.util.PriorityQueue;
+import java.util.Calendar;
 
-public class PriorityQueueTest
-{
-    public static void main(String []args)
-    {
-        System.out.println("Hello World!");
+public class PriorityQueueTest {
+    public static void main(String[] args) {
+        PriorityQueue<String> pq = new PriorityQueue<String>();
+        pq.add("nanjing");
+        pq.add("beijing");
+        while (!(null == pq.peek())) {
+            System.out.println(pq.poll());
+        }
     }
 }
