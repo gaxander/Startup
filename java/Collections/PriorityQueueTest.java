12bce17a40ecdfac8f203cda5670ff1de34e1ac5
ZhouTsing <zhoutsing@foxmail.com>
2019-05-20 23:17:21 +0800

    add PriorityQueueTest.java

---
+++ java/Collections/PriorityQueueTest.java
@@ -0,0 +1,11 @@
+package com.jay.test;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueTest
+{
+    public static void main(String []args)
+    {
+        System.out.println("Hello World!");
+    }
+}
